5d665ac27b79704c9b045e85a2f0f656f61f742f
Damian Mostert <damianmostert86@gmail.com>
2025-03-23 14:56:16 +0200

    updates

--- commands/make/event.js
+++ commands/make/event.js
@@ -1,3 +1,5 @@
+const path = require("path");
 const newMakeScript = require("./lib/newMakeScript");
 const name = process.argv[2];
-console.log(name)
+newMakeScript(name,path.join(__dirname,"./samples/event.ts"),path.join(process.cwd(),`./db/models/${name}Model.ts`))
+newMakeScript(`${name}Event`,path.join(__dirname,"./samples/type.ts"),path.join(process.cwd(),`./types/${name}Event.ts`))
