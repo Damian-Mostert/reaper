8c4ac7da06651d40db5bb9c2c0142b01f601bf04
Damian Mostert <damianmostert86@gmail.com>
2025-03-25 16:13:03 +0200

    updates

--- db/utils.ts
+++ db/utils.ts
@@ -1,28 +1,33 @@
 
 import { Migration } from './lib/migration';
+import query from './lib/query';
 import { migrations } from './models/migration';
 
 export const getRecords = async () => {
-    try {
-        const migrationRecords = await migrations.query().get();
-        return migrationRecords;
-    } catch (error) {
-        await (await Migration("migrations", {
-            up(blueprint){
-                blueprint.id()
-                blueprint.string("name")
-                blueprint.number("batch")
-                blueprint.timestamps()
-            },
-            down(blueprint){
-                blueprint.dropTable()
-            },
-        }))("up")
-        .catch(e=>console.error(e))
-        .then(() => {});
-        const migrationRecords = await migrations.query().get();
-        return migrationRecords;
-    }
+    const tableExists = await query(`
+            SELECT COUNT(*) as count 
+            FROM information_schema.tables 
+            WHERE table_name = 'migrations'
+        `);
+        if (tableExists[0]?.count === 0) {
+            const migrationRecords = await migrations.query().get();
+            return migrationRecords;    
+        }else{
+            await (await Migration("migrations", {
+                up(blueprint){
+                    blueprint.id()
+                    blueprint.string("name")
+                    blueprint.number("batch")
+                    blueprint.timestamps()
+                },
+                down(blueprint){
+                    blueprint.dropTable()
+                },
+            }))("up")
+            .catch(e=>console.error(e))
+            .then(() => {});
+            return [];
+        }
 };
 
 
