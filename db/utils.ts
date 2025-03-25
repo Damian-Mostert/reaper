339b8224e00d47637c525819638830e7e4b37a36
Damian Mostert <damianmostert86@gmail.com>
2025-03-25 16:20:33 +0200

    updates

--- db/utils.ts
+++ db/utils.ts
@@ -1,9 +1,46 @@
 
 import { Migration } from './lib/migration';
-import query from './lib/query';
+import query, { connectionParams } from './lib/query';
 import { migrations } from './models/migration';
+const logger = require("../utils/logger")
+const log = logger;
+// Function to check if the database exists
+async function checkDatabaseExists() {
+    try {
+        const result:any = await query(`SHOW DATABASES LIKE '${connectionParams.database}'`);
+        return result.length > 0;
+    } catch (err) {
+        log.error(`Error checking database: ${err.message}`);
+        return false;
+    }
+}
 
+// Function to create the database if not exists
+async function createDatabaseIfNeeded() {
+    const dbExists = await checkDatabaseExists();
+
+    if (dbExists) {
+        log.success(`Database '${connectionParams.database}' already exists.`);
+    } else {
+        const userResponse = await log.askQuestion(`Database '${connectionParams.database}' does not exist. Do you want to create it?`);
+
+        if (userResponse) {
+            log.startLoading("Creating database...");
+            try {
+                await query(`CREATE DATABASE \`${connectionParams.database}\``);
+                log.stopLoading("Database created successfully!");
+            } catch (err) {
+                log.error(`Error creating database: ${err.message}`);
+            }
+        } else {
+            log.warning("Database creation aborted.");
+        }
+    }
+}
+
+        // Ensure the database exists
 export const getRecords = async () => {
+    await createDatabaseIfNeeded();
     const tableExists = await query(`
             SELECT COUNT(*) as count 
             FROM information_schema.tables 
